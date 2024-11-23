f30b369bb0dd0b046d29888fb152da0997412ad1
womp <24914325+wompscode@users.noreply.github.com>
2024-11-23 14:52:15 +1100

    Actually make the plugin. Rename SamplePlugin to AFKMute.

--- AFKMute/Configuration.cs
+++ AFKMute/Configuration.cs
@@ -1,16 +1,14 @@
-﻿using Dalamud.Configuration;
+using Dalamud.Configuration;
 using Dalamud.Plugin;
 using System;
 
-namespace SamplePlugin;
+namespace AFKMute;
 
 [Serializable]
 public class Configuration : IPluginConfiguration
 {
     public int Version { get; set; } = 0;
-
-    public bool IsConfigWindowMovable { get; set; } = true;
-    public bool SomePropertyToBeSavedAndWithADefault { get; set; } = true;
+    public bool PluginActive { get; set; } = true;
 
     // the below exist just to make saving less cumbersome
     public void Save()
